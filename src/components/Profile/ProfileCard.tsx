b4281038d87fccfec5d89a99a8eaf727b202442a
Anton Efimov <a.ephimoff@gmail.com>
2023-01-13 15:28:16 +0100

    Added a reacl userpic or noavatar component

--- src/components/Profile/ProfileCard.tsx
+++ src/components/Profile/ProfileCard.tsx
@@ -1,25 +1,42 @@
 import Card from '../Card';
+import NoAvatar from '../NoAvatar';
 
 type ProfileCardProps = {
   name: string;
   title: string;
   team: string;
   email: string;
+  userpic: string;
 };
 
-const ProfileCard = ({ name, title, team, email }: ProfileCardProps) => {
+const ProfileCard = ({
+  name,
+  title,
+  team,
+  email,
+  userpic,
+}: ProfileCardProps) => {
   return (
     <Card>
       <div className="flex">
-        <img
-          src="https://thispersondoesnotexist.com/image"
-          alt="profile picture"
-          className="mr-5 h-20 w-20 rounded-full"
-        />
+        <div className="mr-5">
+          {userpic ? (
+            <img
+              src={userpic}
+              alt="profile picture"
+              className="h-20 w-20 rounded-full"
+            />
+          ) : (
+            <NoAvatar name={name} size={20} />
+          )}
+        </div>
+
         <div className="">
           <h2 className="text-sky-500 sm:text-xl md:text-2xl">{name}</h2>
           <h3 className="text-slate-600 sm:text-sm md:text-base">{title}</h3>
-          <h4 className="font-light text-slate-400 sm:text-xs md:text-base">{`${team}, ${email}`}</h4>
+          <h4 className="font-light text-slate-400 sm:text-xs md:text-base">
+            {team ? `${team}, ` : null} {email}
+          </h4>
         </div>
       </div>
     </Card>
