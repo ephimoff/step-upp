c619387adcce484de8a2fdb9a262cb1ec1db4135
Anton Efimov <a.ephimoff@gmail.com>
2023-01-26 13:22:44 +0100

    Added external slug validation

--- src/schemas/validationSchemas.ts
+++ src/schemas/validationSchemas.ts
@@ -3,7 +3,14 @@ import * as yup from 'yup';
 export const profileSchema = yup.object().shape({
   name: yup.string().required('Required'),
   email: yup.string().email('Please enter a valid email').required('Required'),
-  slug: yup.string().required('Required'),
+  slug: yup
+    .string()
+    .required('Required')
+    .test('Unique slug', 'Slug is already in use', async (value) => {
+      const response = await fetch(`/api/profile?slug=${value}`);
+      console.log(response);
+      return response.ok ? false : true;
+    }),
   phone: yup.string(),
   twitter: yup.string(),
   linkedin: yup.string().url('The field must be a valid URL'),
