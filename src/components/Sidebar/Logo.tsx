be683605c86308d205e1d1499239b59510410fd4
Anton Efimov <a.ephimoff@gmail.com>
2023-02-21 20:57:25 +0100

    cleanup

--- src/components/Sidebar/Logo.tsx
+++ src/components/Sidebar/Logo.tsx
@@ -1,18 +1,14 @@
-// import { HiOutlineBolt } from 'react-icons/hi2';
-import { Carrot } from 'lucide-react';
-import logo0 from '@/assets/logo0.png';
+// import logo0 from '@/assets/logo0.png';
 import logo1 from '@/assets/logo1.png';
 import logo2 from '@/assets/logo.png';
 import logo1Dark from '@/assets/logo1_dark.png';
 import Image from 'next/image';
 
-type LogoProps = {
+type Props = {
   open: boolean;
 };
 
-//
-
-const Logo = ({ open }: LogoProps) => {
+const Logo = ({ open }: Props) => {
   return (
     <div className="flex items-center gap-x-4">
       <Image
@@ -27,28 +23,13 @@ const Logo = ({ open }: LogoProps) => {
         }`}
         alt=""
       />
-      {/* <span
-        className={`rounded-xl bg-gradient-to-r from-[#12c2e9] via-purple-700 to-[#f64f59] p-2 text-3xl text-white duration-500 ${
-          open && 'rotate-[360deg]'
-        }`}
-      >
-        <Carrot className="duration-500" size={32} />
-      </span> */}
       <Image
         src={logo2}
-        className={`w-36 shrink-0 origin-left duration-500 ${
+        className={`w-36 shrink-0 origin-left duration-300 ${
           !open && 'scale-0'
         }`}
         alt=""
       />
-      {/* <h1
-        className={`${
-          !open && 'scale-0'
-        } origin-left whitespace-nowrap bg-gradient-to-r from-[#12c2e9] via-purple-700 to-[#f64f59] bg-clip-text text-3xl font-medium text-transparent duration-300`}
-      >
-        Step
-        <strong className={`font-extrabold`}>Upp</strong>
-      </h1> */}
     </div>
   );
 };
