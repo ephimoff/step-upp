40b2f20c1b787c4a70cb8812448051e279158acb
Anton Efimov <a.ephimoff@gmail.com>
2023-03-02 10:27:03 +0100

    fixing auth issue

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -31,7 +31,7 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
-
+      checks: ['pkce', 'state'],
       authorization: {
         params: {
           prompt: 'consent',
