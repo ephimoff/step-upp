30e1540df0863440eeac73696506209d000aea40
Anton Efimov <a.ephimoff@gmail.com>
2023-03-20 21:56:18 +0100

    added loading workspace data

--- src/pages/workspace.tsx
+++ src/pages/workspace.tsx
@@ -1,20 +1,38 @@
-import type { ProfileType } from '@/types/types';
 import type { GetServerSidePropsContext } from 'next';
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '@/pages/api/auth/[...nextauth]';
 import Sidebar from '@/components/Sidebar/Sidebar';
 import prisma from '@/utils/prisma';
 import Card from '@/components/Card';
+import FormikForm from '@/components/FormikForm';
 
 type Props = {
-  profile: ProfileType;
+  profile: any;
 };
 
 const WorkspacePage = ({ profile }: Props) => {
+  console.log('profile', profile);
+  const fields = [
+    {
+      label: 'Workspace',
+      value: profile.user.membership[0].workspace.name,
+    },
+    {
+      label: 'Description',
+      value: profile.user.membership[0].workspace.description || '',
+    },
+  ];
   return (
     <>
       <Sidebar name={profile.name}>
-        <Card>Workspace</Card>
+        <Card>
+          <h1>Workspace</h1>
+          <h2>About</h2>
+          <FormikForm fields={fields} />
+          <pre className="text-xs font-thin text-black dark:text-white">
+            {JSON.stringify(profile, null, 2)}
+          </pre>
+        </Card>
       </Sidebar>
     </>
   );
@@ -45,6 +63,9 @@ export const getServerSideProps = async ({
     where: {
       email: session!.user!.email as string,
     },
+    include: {
+      user: { include: { membership: { include: { workspace: true } } } },
+    },
   });
 
   if (!profile) {
@@ -55,6 +76,7 @@ export const getServerSideProps = async ({
       },
     };
   }
+  console.log('profile.user.membership', profile!.user.membership);
   console.info(`${PAGE} page - Profile found`);
   console.debug(`${PAGE} page - Profile: `, profile);
 
